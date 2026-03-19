33aa09227c9f45771a3f032a2ac0e495768a0808
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2026-03-20 00:33:21 +0530

    Updated TTL config for caching

--- src/main/java/com/sabarno/chatomania/config/RedisCacheConfig.java
+++ src/main/java/com/sabarno/chatomania/config/RedisCacheConfig.java
@@ -1,11 +1,15 @@
 package com.sabarno.chatomania.config;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -28,6 +32,31 @@ public class RedisCacheConfig {
                 )
                 .disableCachingNullValues()
                 .entryTtl(Duration.ofMinutes(5));
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(
+            RedisConnectionFactory connectionFactory,
+            RedisCacheConfiguration defaultConfig
+    ){
+        Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+
+        cacheConfigs.put("users",
+                defaultConfig.entryTtl(Duration.ofMinutes(10)));
+
+        cacheConfigs.put("chatList",
+                defaultConfig.entryTtl(Duration.ofMinutes(5)));
+
+        cacheConfigs.put("chatParticipants",
+                defaultConfig.entryTtl(Duration.ofMinutes(20)));
+
+        cacheConfigs.put("presence",
+                defaultConfig.entryTtl(Duration.ofSeconds(30)));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
     }
 }
 
